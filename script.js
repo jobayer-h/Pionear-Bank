c47919f4d220097f209c83e2a3eaed9fd4a3aed1
jobayer-h <jobayer.dev@gmail.com>
2020-07-24 22:12:02 +0600

    Website is Functional Now

--- script.js
+++ script.js
@@ -8,3 +8,40 @@ loginBtn.addEventListener('click',function(){
     const transitionArea = document.getElementById('transition-area');
     transitionArea.style.display = 'block';
 })
+
+//Diposite Calculation
+
+const depositBtn = document.getElementById('deposit-btn');
+depositBtn.addEventListener('click',function(){
+    const input = inputDataRead('deposit-input');
+    const balance = balanceDataRead('deposit');
+    const total = input + balance;
+    document.getElementById('deposit').innerText = total;
+    const currentBalance = balanceDataRead('balance');
+    const updateCurrentBalance = currentBalance + input;
+    document.getElementById('balance').innerText = updateCurrentBalance;
+})
+//withdraw calculation
+const withrawBtn = document.getElementById('withdraw-btn');
+withrawBtn.addEventListener('click', function(){
+    const input = inputDataRead('withdraw-input');
+    const balance = balanceDataRead('withdraw');
+    const total = input + balance;
+    document.getElementById('withdraw').innerText = total;
+    const currentBalance = balanceDataRead('balance');
+    const updateCurrentBalance = currentBalance - input;
+    document.getElementById('balance').innerText = updateCurrentBalance;
+})
+
+//functions
+function inputDataRead(inputID){
+    const dataRead = document.getElementById(inputID).value;
+    const dataReadValue = parseFloat(dataRead);
+    document.getElementById(inputID).value = '';
+    return dataReadValue;
+}
+function balanceDataRead(inputID){
+    const dataRead = document.getElementById(inputID).innerText;
+    const dataReadValue = parseFloat(dataRead);
+    return dataReadValue;
+}
