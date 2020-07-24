d4925d8c8ca559d4db55b56bbc5937be038dd653
jobayer-h <jobayer.dev@gmail.com>
2020-07-24 22:57:07 +0600

    final commit

--- script.js
+++ script.js
@@ -14,23 +14,31 @@ loginBtn.addEventListener('click',function(){
 const depositBtn = document.getElementById('deposit-btn');
 depositBtn.addEventListener('click',function(){
     const input = inputDataRead('deposit-input');
-    const balance = balanceDataRead('deposit');
-    const total = input + balance;
-    document.getElementById('deposit').innerText = total;
-    const currentBalance = balanceDataRead('balance');
-    const updateCurrentBalance = currentBalance + input;
-    document.getElementById('balance').innerText = updateCurrentBalance;
+    if (input > 0) {
+        const balance = balanceDataRead('deposit');
+        const total = input + balance;
+        updateBalance('deposit',total);
+        const currentBalance = balanceDataRead('balance');
+        const updateCurrentBalance = currentBalance + input;
+        updateBalance('balance',updateCurrentBalance);
+    }else{
+        alert('Please enter a valid Number');
+    }
 })
 //withdraw calculation
 const withrawBtn = document.getElementById('withdraw-btn');
 withrawBtn.addEventListener('click', function(){
     const input = inputDataRead('withdraw-input');
-    const balance = balanceDataRead('withdraw');
-    const total = input + balance;
-    document.getElementById('withdraw').innerText = total;
-    const currentBalance = balanceDataRead('balance');
-    const updateCurrentBalance = currentBalance - input;
-    document.getElementById('balance').innerText = updateCurrentBalance;
+    if (input > 0) {
+        const balance = balanceDataRead('withdraw');
+        const total = input + balance;
+        updateBalance('withdraw',total);
+        const currentBalance = balanceDataRead('balance');
+        const updateCurrentBalance = currentBalance - input;
+        updateBalance('balance',updateCurrentBalance);
+    }else{
+        alert('Please enter a valid Number');
+    }
 })
 
 //functions
@@ -44,4 +52,7 @@ function balanceDataRead(inputID){
     const dataRead = document.getElementById(inputID).innerText;
     const dataReadValue = parseFloat(dataRead);
     return dataReadValue;
+}
+function updateBalance(inputID,total) {
+    document.getElementById(inputID).innerText = total;
 }
